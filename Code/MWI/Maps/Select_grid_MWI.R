bf0a3d19abbe2eef3d215ff61ee504b662a70862
Michiel van Dijk <michiel.vandijk@wur.nl>
2017-08-10 08:50:10 +0200

    Added model description Added second approach to add border cells - experimental

--- Code/MWI/Maps/Select_grid_MWI.R
+++ Code/MWI/Maps/Select_grid_MWI.R
@@ -57,7 +57,19 @@ grid <- mask(grid, adm1)
 plot(grid)
 names(grid) <- "gridID"
 
-### APPROACH TO ADD BORDER CELLS
+# Write raster
+saveRDS(grid, file.path(dataPath, "Data/MWI/Processed/Maps/grid_r_MWI.rds"))
+
+# Create polygon
+grid_py <- rasterToPolygons(grid)
+plot(grid_py)
+plot(adm1, add = T, border = "red")
+
+# Write polygon
+saveRDS(grid_py, file.path(dataPath, "Data/MWI/Processed/Maps/grid_MWI.rds"))
+
+
+### APPROACH TO ADD BORDER CELLS #1
 # Note that:
 # 1. We need another version of adm0 that excludes lake MWI => union adm1
 # 2. Any next steps that include the (total crop) area of the grid cell requires special treatment of the 
@@ -75,16 +87,27 @@ plot(r2)
 plot(adm1, add = TRUE)
 x <- rasterToPoints(r2)
 
-# Write raster
-saveRDS(grid, file.path(dataPath, "Data/MWI/Processed/Maps/grid_r_MWI.rds"))
+### APPROACH TO ADD BORDER CELLS #2
+# Still have to check if it works
+# Note that:
+# 1. We need another version of adm0 that excludes lake MWI => union adm1
+# 2. Any next steps that include the (total crop) area of the grid cell requires special treatment of the 
+# border cells => potential approach is to polygize the raster, crop to adm0 and calculate area of border cells
+# However all this might be a lot of extra work for very limited returns.
 
+adm0 <- readRDS(file.path(dataPath, "Data/MWI/Processed/Maps/GAUL_MWI_adm0_2000.rds"))
+plot(adm0)
+weights <- raster::extract(r, adm0, df = T, weights = T, normalizeWeights = F) %>%
+  setNames(c("polyID", "gridID", "weight"))
 
-# Create polygon
-grid_py <- rasterToPolygons(grid)
-plot(grid_py)
-plot(adm1, add = T, border = "red")
+gridID_df <- as.data.frame(rasterToPoints(grid))
+check <- left_join(weights, gridID_df) 
 
-# Write polygon
-saveRDS(grid_py, file.path(dataPath, "Data/MWI/Processed/Maps/grid_MWI.rds"))
-
+check_r <- r
+check_r[!(check_r %in% check$gridID)] <- NA
+plot(check_r)
+plot(adm0)
+plot(check_r, add = T)
+plot(grid_py, add = T, border = "red")
+plot(adm0, add = T, border = "red")
 
